24737c97c99157e5faa714cb6e03dacfe50c5140
ranojulius <ranojulius05@gmail.com>
2023-11-19 06:45:55 -0800

    first entry

---
+++ registrationapp/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using registrationapp.Models;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace registrationapp.Controllers
+{
+    public class UserController : Controller
+    {
+        SqlConnection conn = new SqlConnection();
+        SqlCommand cmd = new SqlCommand();
+        SqlDataAdapter Reader;
+        // GET: User
+        [HttpGet]
+        public ActionResult Login()
+        {
+            ViewBag.Message = TempData["Message"];
+            return View();
+        }
+        [HttpPost]
+        public ActionResult verify(Users usermodel)
+        {
+            conn.ConnectionString = "Data Source=DESKTOP-R9IOJR3;Initial Catalog=registrationdb;Integrated Security=True;Pooling=False;MultipleActiveResultSets=True;Application Name=EntityFramework";
+            conn.Open();
+            cmd.Connection = conn;
+            cmd.CommandText="select *from registrationtbl"
+        }
+
+    }
+}
